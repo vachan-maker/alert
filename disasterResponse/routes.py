e46b4690c1a733e12b84edaec0517501541261e3
vachan-maker <vachan@tutanota.com>
2025-02-12 22:15:19 +0530

    UI Stuff

--- disasterResponse/routes.py
+++ disasterResponse/routes.py
@@ -1,6 +1,15 @@
 from flask import render_template, request, jsonify
+from disasterResponse import supabase
 from disasterResponse import app
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/sos")
+def sos():
+    response = (
+    supabase.table("SOS Alerts")
+    .insert({"user_identification": 8921385972, "message": "Pluto"})
+    .execute()
+)
