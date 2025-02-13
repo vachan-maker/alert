87711697fa3b5187876cc05abc8a9308f4578800
vachan-maker <vachan@tutanota.com>
2025-02-13 17:14:42 +0530

    Added signup page

--- disasterResponse/routes.py
+++ disasterResponse/routes.py
@@ -26,6 +26,20 @@ def login():
         return "You are logged in"
     except Exception as e:
         return "Wrong email or password"
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            response = supabase.auth.sign_up(
+    {"email": email, "password": password}
+)
+            print(response)
+            return "You are registered"
+        except Exception as e:
+            return "User already exists"
+    return render_template("register.html")
     
 @app.route("/")
 def home():
